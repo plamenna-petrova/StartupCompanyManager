d032b02b58201ba29c5be78beada7a8ae6c8d952
plamenna.petrova <plamennavp@abv.bg>
2023-12-29 13:25:01 +0200

    Added Generic Repository pattern starter logic

---
+++ StartupCompanyManager/Infrastructure/Repositories/Contracts/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupCompanyManager.Infrastructure.Repositories.Contracts
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        ICollection<TEntity> GetAll();
+
+        ICollection<TEntity> GetAllByCondition(Func<TEntity, bool> filterExpression);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity, string propertyName, object propertyValueToSet);
+
+        void Delete(TEntity entity);
+
+        bool Exists(TEntity entityToFind);
+    }
+}
---
+++ StartupCompanyManager/Infrastructure/Repositories/Contracts/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using StartupCompanyManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupCompanyManager.Infrastructure.Repositories.Contracts
+{
+    public interface IDepartmentRepository : IBaseRepository<Department>
+    {
+
+    }
+}
